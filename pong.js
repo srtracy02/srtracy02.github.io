9ffed61afc8ab4541e1e5937317b616aa2162400
srtracy02 <46983168+srtracy02@users.noreply.github.com>
2021-05-07 14:55:07 -0400

    Update pong.js

--- pong.js
+++ pong.js
@@ -28,3 +28,60 @@ var render = function() {
   context.fillStyle = "#FF00FF";
   context.fillRect(0, 0, width, height);
 };
+
+function Paddle(x, y, width, height) {
+  this.x = x;
+  this.y = y;
+  this.width = width;
+  this.height = height;
+  this.x_speed = 0;
+  this.y_speed = 0;
+}
+
+Paddle.prototype.render = function() {
+  context.fillStyle = "#0000FF";
+  context.fillRect(this.x, this.y, this.width, this.height);
+};
+
+function Player() {
+   this.paddle = new Paddle(175, 580, 50, 10);
+}
+
+function Computer() {
+  this.paddle = new Paddle(175, 10, 50, 10);
+}
+
+Player.prototype.render = function() {
+  this.paddle.render();
+};
+
+Computer.prototype.render = function() {
+  this.paddle.render();
+};
+
+function Ball(x, y) {
+  this.x = x;
+  this.y = y;
+  this.x_speed = 0;
+  this.y_speed = 3;
+  this.radius = 5;
+}
+
+Ball.prototype.render = function() {
+  context.beginPath();
+  context.arc(this.x, this.y, this.radius, 2 * Math.PI, false);
+  context.fillStyle = "#000000";
+  context.fill();
+};
+
+var player = new Player();
+var computer = new Computer();
+var ball = new Ball(200, 300);
+
+var render = function() {
+  context.fillStyle = "#FF00FF";
+  context.fillRect(0, 0, width, height);
+  player.render();
+  computer.render();
+  ball.render();
+};
