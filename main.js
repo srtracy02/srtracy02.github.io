b29d4d1b97e2a6ba72671dcb06c9c1806f999950
srtracy02 <46983168+srtracy02@users.noreply.github.com>
2021-05-07 21:37:10 -0400

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-var canvas = document.createElement("canvas"),
-    width = canvas.width = window.innerWidth,
-    height = canvas.height = window.innerHeight;
-
-document.body.appendChild(canvas);
-
-var ctx = canvas.getContext("2d");
-
-ctx.fillRect(0, 0, width, height);
