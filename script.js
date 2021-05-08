2a2e2127c45d0b293da4840e759f46c03ad25004
srtracy02 <46983168+srtracy02@users.noreply.github.com>
2021-05-07 21:27:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
 const canvas=document.getElementById('canvas');
 const context=canvas.getContext('2d');
 
-var username;
-var usercolor;
-
 function renderCanvas() {
   context.fillStyle="#000";
   context.fillRect(0,0,canvas.width,canvas.height);
 }
 
 renderCanvas();
-
-function startScreen() {
-  $("#canvas".append(
-    $('<img>.attr({id:"start-game", src: 'accessories/start_game.png'}));
-}
