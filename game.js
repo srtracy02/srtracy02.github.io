b8f7a456479034bba1f76803ce59455950b9e1c9
srtracy02 <46983168+srtracy02@users.noreply.github.com>
2021-05-08 23:55:10 -0400

    Update game.js

--- game.js
+++ game.js
@@ -228,7 +228,7 @@ function getCompliment() {
         method: 'get',
         url: 'https://complimentr.com/api',
     });
-    alert(comp.data.compliment)
+    alert("UGH")
 }
         
 function getInsult() {
