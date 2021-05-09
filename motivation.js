3f785d825a4ca756dacaf1c8830ec7ae9f8343d0
srtracy02 <46983168+srtracy02@users.noreply.github.com>
2021-05-09 00:48:52 -0400

    Update motivation.js

--- motivation.js
+++ motivation.js
@@ -1,6 +1,6 @@
 export const renderComplimentButton = function() {
     return `<div style="text-align:center;">
-        <button class="button compliment-button" style="position: relative; top: 400px;" >Motivate Me</button>
+        <button class="button compliment-button" style="position: relative; top: 500px;" >Motivate Me</button>
     </div>
     `
 }
@@ -11,15 +11,15 @@ export const handleComplimentButtonPress = async function(event) {
         method: 'get',
         url: 'https://complimentr.com/api',
     });
-    $(root).append(`<div style="text-align: center; font-size: 36px; position: relative; top: 500px;">
-        <h1>${comp.data.compliment}</h1>
+    $(root).append(`<div style="text-align: center; font-size: 36px; position: relative; top: 600px;">
+        <h3>${comp.data.compliment}</h3>
     </div>`)
 }
 
 export  const loadComplimentIntoDOM = async function() {
     // Grab a jQuery reference to the root HTML element
     const $root = $('#root');
-    $root.append(`<div style="text-align: center; font-size: 36px; position: relative; top: 450px;">
+    $root.append(`<div style="text-align: center; font-size: 36px; position: relative; top: 550px;">
         <a href="./index.html">Clear Motivation</a>
     </div>`)
     $root.append(renderComplimentButton());
