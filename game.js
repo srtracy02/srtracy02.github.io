d7e1a6802047f041784600564d7987f2ae8feadb
srtracy02 <46983168+srtracy02@users.noreply.github.com>
2021-05-09 00:57:15 -0400

    Update game.js

--- game.js
+++ game.js
@@ -33,38 +33,6 @@ const board = () => {
     ctx.stroke()
 }
 
-/*const renderComplimentButton = function() {
-    return `<div style="text-align:center;">
-        <button class="button compliment-button">GIVE ME AFFIRMATION</button>
-    </div>
-    `
-}
-
-const handleComplimentButtonPress = async function(event) {
-    const $root = $('#root');
-    const comp = await axios({
-        method: 'get',
-        url: 'https://complimentr.com/api',
-    });
-    $(root).append(`<div style="text-align: center; font-size: 80px;">
-        <h1>${comp.data.compliment}</h1>
-    </div>`)
-}
-
-const loadComplimentIntoDOM = async function() {
-    const $root = $('#root');
-    $root.append(`<div style="text-align: center; font-size: 100px;">
-        <a href="./index.html">Back to Main</a>
-    </div>`)
-    $root.append(renderComplimentButton());
-    $root.on("click", ".compliment-button", handleComplimentButtonPress);
-}
-
-$(function() {
-    loadComplimentIntoDOM();
-});
-*/
-
 window.addEventListener('mousemove', (e) => {
     player.x = e.x - window.innerWidth/2 + w/2
     player.y = e.y - h*.05
