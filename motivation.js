6036d7395a2ee0e9ec15bb51ea869c0bb331d2f9
srtracy02 <46983168+srtracy02@users.noreply.github.com>
2021-05-09 00:55:33 -0400

    Update motivation.js

--- motivation.js
+++ motivation.js
@@ -1,15 +1,19 @@
-export const renderComplimentButton = function() {
+export const renderMotivationButton = function() {
     return `<div style="text-align:center;">
-        <button class="button compliment-button" style="position: relative; top: 500px;" >Motivate Me</button>
+        <button class="button motivation-button" style="position: relative; top: 500px;" >Motivate Me</button>
     </div>
     `
 }
 
-export const handleComplimentButtonPress = async function(event) {
+export const handleMotivationButtonPress = async function(event) {
     const $root = $('#root');
     const comp = await axios({
         method: 'get',
         url: 'https://complimentr.com/api',
+    });
+    const ins = await axios({
+        method: 'get',
+        url: 'https://insult.mattbas.org/api/insult',
     });
     $(root).append(`<div style="text-align: center; font-size: 36px; position: relative; top: 600px;">
         <h3>${comp.data.compliment}</h3>
